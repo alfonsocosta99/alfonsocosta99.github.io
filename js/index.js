1a4bbf9ea8fa08762006c9ee1aa279fac76bf335
alfonsocosta99 <costa19alfonso99@gmail.com>
2022-08-21 18:52:00 -0300

    entrega 1

--- js/index.js
+++ js/index.js
@@ -1,14 +1,34 @@
-document.addEventListener("DOMContentLoaded", function(){
-    document.getElementById("autos").addEventListener("click", function() {
-        localStorage.setItem("catID", 101);
-        window.location = "products.html"
-    });
-    document.getElementById("juguetes").addEventListener("click", function() {
-        localStorage.setItem("catID", 102);
-        window.location = "products.html"
-    });
-    document.getElementById("muebles").addEventListener("click", function() {
-        localStorage.setItem("catID", 103);
-        window.location = "products.html"
-    });
+let formulario = document.getElementById('formulario-login');
+
+let emailInput = document.getElementById('email-input');
+
+let passInput = document.getElementById('pass-input');
+
+let textoEmail = emailInput.value;
+
+let textoPass = passInput.value;
+
+formulario.addEventListener('submit', function(e) {
+    let textoEmail = emailInput.value;
+    let textoPass = passInput.value;
+
+    if(textoEmail.length < 1 || textoPass.length < 1) {
+        e.preventDefault();
+        if(textoEmail.length < 1 && textoPass.length < 1) {
+            document.getElementById('alerta-email').innerHTML = "Debes ingresar tu email";
+            document.getElementById('alerta-pass').innerHTML = "Debes ingresar tu contraseña";
+            document.getElementById('email-input').style.border= "3px solid #FF0000";
+            document.getElementById('pass-input').style.border= "3px solid #FF0000";
+        } else if(textoEmail.length < 1){
+            document.getElementById('alerta-email').innerHTML = "Debes ingresar tu email";
+            document.getElementById('alerta-pass').innerHTML = "";
+            document.getElementById('email-input').style.border= "3px solid #FF0000";
+            document.getElementById('pass-input').style.border= "";
+        } else {
+            document.getElementById('alerta-pass').innerHTML = "Debes ingresar tu contraseña";
+            document.getElementById('alerta-email').innerHTML = "";
+            document.getElementById('pass-input').style.border= "3px solid #FF0000";
+            document.getElementById('email-input').style.border= "";
+        }
+    }
 });
